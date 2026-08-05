fa0e41cc537890a243e8f47ebb21e8ec0453e3e1
jer91big <2975664614@qq.com>
2026-08-05 21:15:10 +0800

    Cleanup: remove music proxy/debug endpoints, keep playlist only

--- api/routes/music.ts
+++ api/routes/music.ts
@@ -1,10 +1,8 @@
 import express from 'express';
-import { getPlaylist, proxySong, debugSong } from '../controllers/musicController.js';
+import { getPlaylist } from '../controllers/musicController.js';
 
 const router = express.Router();
 
 router.get('/playlist', getPlaylist);
-router.get('/proxy', proxySong);
-router.get('/debug', debugSong);
 
 export default router;
